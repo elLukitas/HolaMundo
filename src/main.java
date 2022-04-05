b9a34ea6b53f0238fa9bab749108faf454656acf
Víctor Fernandes <91089588+elLukitas@users.noreply.github.com>
2022-04-05 20:18:41 +0200

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("palomitas de maiz");
+        System.out.println("pancho");
+        System.out.printl("sudaca");
+        
     }
 }
